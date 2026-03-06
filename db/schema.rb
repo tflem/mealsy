4c66e5cf42132a9ae9a72cbe4cdde05765623823
Tim Fleming <tfleming26@hotmail.com>
2026-03-05 18:39:59 -0600

    Update ruby config

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2022_08_21_160537) do
+ActiveRecord::Schema[8.1].define(version: 2022_08_21_160537) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "meals", force: :cascade do |t|
-    t.string "name", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
     t.datetime "updated_at", null: false
   end
 end
